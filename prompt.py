35a4399e66e1dee0cf00689d0b9ae0aa9d9ac6d5
jnchen <42629624@qq.com>
2023-07-19 22:08:58 +0800

    update: 因为chatgpttoken限制，只取top相似知识

--- prompt.py
+++ prompt.py
@@ -4,13 +4,19 @@
 # 主要生成提示词
 
 chatglm_template = '已知信息:\n {}\n 根据已知信息回答问题:\n {}'
+chatgpt_template = '已知信息:\n {}\n 请根据已知信息回答问题,不要回答已经信息中没有的内容，问题是:\n {}'
 
 class Prompt:
     def __init__(self, llm):
         self.llm = llm
 
     def build(self, content, query):
-        if isinstance(content, list):
-            content = '\n'.join(content)
         if self.llm == 'chatglm2-6b':
+            if isinstance(content, list):
+                content = '\n'.join(content)
             return chatglm_template.format(content, query)
+        elif self.llm == 'chatgpt':
+            # chatgpt的token宽度只有4096,只取top1
+            if isinstance(content, list):
+                content = content[0]
+            return chatgpt_template.format(content, query)
